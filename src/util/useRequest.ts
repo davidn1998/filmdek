37c91a8fcca907e47cc9ff9476e8b4528875f333
David Nwachukwu <davidnw1998@gmail.com>
2023-02-21 17:44:14 +0000

    remove api key from repo

--- src/util/useRequest.ts
+++ src/util/useRequest.ts
@@ -3,12 +3,13 @@ import axios from "axios";
 import useSWR from "swr";
 
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+const TMDB_API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
 export const useGetMovieData = (
   movieId: number | undefined
 ): { movieData: IMovie; movieError: any } => {
   const url = movieId
-    ? `https://api.themoviedb.org/3/movie/${movieId}?api_key=171dbbdf31276d0a3c4a41f542b9717c`
+    ? `https://api.themoviedb.org/3/movie/${movieId}?api_key=${TMDB_API_KEY}`
     : "";
 
   const { data: movieData, error: movieError } = useSWR(url, fetcher, {
@@ -24,7 +25,7 @@ export const useGetMovieSearch = (
   year?: number | undefined
 ): { searchData: ISearch; searchError: any } => {
   const url = searchQuery
-    ? `https://api.themoviedb.org/3/search/movie?api_key=171dbbdf31276d0a3c4a41f542b9717c&query=${searchQuery}&page=${pageNum}&include_adult=false${
+    ? `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&query=${searchQuery}&page=${pageNum}&include_adult=false${
         year ? "&year=" + year : ""
       }`
     : null;
